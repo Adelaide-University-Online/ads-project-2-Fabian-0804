6bf27be61ee4b82e9eee3232d971ff514f19320a
Fabian <diyfy006@mymail.unisa.edu.au>
2026-05-28 11:38:27 +1000

    Implement Course class

---
+++ src/main/java/Course.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Course {
+
+    // Stores the course code
+    private String code;
+
+    public Course(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        // Checks if both objects are the same
+        if (this == obj) {
+            return true;
+        }
+
+        // Checks if the object is null or a different class
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Course course = (Course) obj;
+
+        // Compares course codes
+        return Objects.equals(code, course.code);
+    }
+
+    @Override
+    public int hashCode() {
+
+        // Generates hash code using the course code
+        return Objects.hash(code);
+    }
+}
